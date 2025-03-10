31e77ccb6b6ad26d268fe115be4cd76716ee7d35
Carolz87 <carolfranco.oliveira@gmail.com>
2025-03-10 19:18:20 -0300

    Initial frontend structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ChatComponent from "./components/ChatComponent.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Frontend Vue + Vite 🚀</h1>
+    <ChatComponent />
+  </div>
+</template>
